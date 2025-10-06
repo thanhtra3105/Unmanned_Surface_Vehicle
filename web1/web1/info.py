938ffbe17baea3cead46cd999af80c600216a3ca
xtruong101 <truong13062k4@gmail.com>
2025-10-06 19:50:13 +0700

    dev

--- web1/web1/info.py
+++
@@ -1,8 +0,0 @@
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'gcsdutteam@gmail.com'
-EMAIL_HOST_PASSWORD = 'wfqv uect ksja uraj'  # Use app password if 2FA enabled
-
-PASSWORD_RESET_TIMEOUT = 600
